10bee9cc58e97b77b0f2cdb56a23d0f0fc194793
Falko Schumann <falko.schumann@muspellheim.de>
2020-12-09 20:11:03 +0100

    [Backend Server] Swagger UI integriert

--- todomvc-backend-server/src/main/java/de/muspellheim/todomvc/backend/server/TodoMvcApplication.java
+++ todomvc-backend-server/src/main/java/de/muspellheim/todomvc/backend/server/TodoMvcApplication.java
@@ -7,11 +7,14 @@ package de.muspellheim.todomvc.backend.server;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import javax.enterprise.context.Dependent;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @Dependent
-@ApplicationPath("api")
-@OpenAPIDefinition(info = @Info(version = "1.0", title = "TodoMVC API"))
+@ApplicationPath("/api")
+@OpenAPIDefinition(
+    info = @Info(version = "1.0", title = "TodoMVC API"),
+    servers = @Server(url = "/api"))
 public class TodoMvcApplication extends Application {}
