8de00529fb46af73837de8e50601575816ad82dc
Falko Schumann <falko.schumann@muspellheim.de>
2021-01-03 14:39:03 +0100

    [Build] Code Formatting

--- buildSrc/src/main/groovy/ApplicationUberJarPlugin.groovy
+++ buildSrc/src/main/groovy/ApplicationUberJarPlugin.groovy
@@ -7,49 +7,49 @@ import org.gradle.api.tasks.JavaExec
 import org.gradle.api.tasks.bundling.Jar
 
 class ApplicationUberJarPluginExtension {
-    String mainClassName
+  String mainClassName
 }
 
 class ApplicationUberJarPlugin implements Plugin<Project> {
-    void apply(Project project) {
-        project.pluginManager.apply('todomvc.java-common-conventions')
-        project.pluginManager.apply('distribution')
+  void apply(Project project) {
+    project.pluginManager.apply('todomvc.java-common-conventions')
+    project.pluginManager.apply('distribution')
 
-        def extension = project.extensions.create('application', ApplicationUberJarPluginExtension)
-        project.tasks.register('run', JavaExec) {
-            group = 'application'
-            main = extension.mainClassName
-            classpath = project.convention.getPlugin(JavaPluginConvention).sourceSets.main.runtimeClasspath
-        }
-
-        def uberJar = project.tasks.register('uberJar', Jar) {
-            group = 'distribution'
-            archiveClassifier = 'uber'
-            manifest {
-                attributes(
-                        'Implementation-Title': 'TodoMVC',
-                        'Implementation-Version': archiveVersion,
-                        'Implementation-Vendor': 'Falko Schumann <falko.schumann@muspellheim.de>',
-                        'Main-Class': extension.mainClassName
-                )
-            }
-            from project.convention.getPlugin(JavaPluginConvention).sourceSets.main.output
-            dependsOn project.configurations.runtimeClasspath
-            from {
-                project.configurations.runtimeClasspath.findAll { it.name.endsWith('jar') }.collect { project.zipTree(it) }
-            }
-        }
-
-        def distributions = project.getExtensions().getByName("distributions");
-        Distribution mainDistribution = distributions.getByName(DistributionPlugin.MAIN_DISTRIBUTION_NAME);
-        mainDistribution.contents {
-            from(uberJar)
-            from(project.rootDir) {
-                include 'README.md'
-                include 'LICENSE.txt'
-                include 'CHANGELOG.md'
-                rename 'md', 'txt'
-            }
-        }
+    def extension = project.extensions.create('application', ApplicationUberJarPluginExtension)
+    project.tasks.register('run', JavaExec) {
+      group = 'application'
+      main = extension.mainClassName
+      classpath = project.convention.getPlugin(JavaPluginConvention).sourceSets.main.runtimeClasspath
     }
+
+    def uberJar = project.tasks.register('uberJar', Jar) {
+      group = 'distribution'
+      archiveClassifier = 'uber'
+      manifest {
+        attributes(
+          'Implementation-Title': 'TodoMVC',
+          'Implementation-Version': archiveVersion,
+          'Implementation-Vendor': 'Falko Schumann <falko.schumann@muspellheim.de>',
+          'Main-Class': extension.mainClassName
+        )
+      }
+      from project.convention.getPlugin(JavaPluginConvention).sourceSets.main.output
+      dependsOn project.configurations.runtimeClasspath
+      from {
+        project.configurations.runtimeClasspath.findAll { it.name.endsWith('jar') }.collect { project.zipTree(it) }
+      }
+    }
+
+    def distributions = project.getExtensions().getByName("distributions");
+    Distribution mainDistribution = distributions.getByName(DistributionPlugin.MAIN_DISTRIBUTION_NAME);
+    mainDistribution.contents {
+      from(uberJar)
+      from(project.rootDir) {
+        include 'README.md'
+        include 'LICENSE.txt'
+        include 'CHANGELOG.md'
+        rename 'md', 'txt'
+      }
+    }
+  }
 }
